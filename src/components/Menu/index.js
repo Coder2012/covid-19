a6403ce12b3629e2b74237ae21b1301f4a1a2b8f
Neil Brown <neibrown08@hotmail.com>
2020-04-13 13:49:51 +0100

    styling

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useStore } from 'effector-react'
+import classNames from 'classnames'
 import { category, setCategory } from '../../services/menu'
+
+import styles  from './index.module.scss'
 
 const data = [
   {
@@ -17,12 +20,18 @@ const data = [
   },
 ]
 
-export const Menu = props => {
+export const Menu = (props) => {
   const selectedId = useStore(category)
   return (
-    <nav>
+    <nav className={classNames(styles.menu)}>
       {data.map(({ id, label }) => (
-        <button className={`menu-button ${selectedId === id ? 'selected' : ''}`} type="button" key={id} id={id} onClick={e => setCategory(e.currentTarget.id)}>
+        <button
+          className={classNames(styles[`menu-button`], {[styles[`menu-button--selected`]]: selectedId === id})}
+          type="button"
+          key={id}
+          id={id}
+          onClick={(e) => setCategory(e.currentTarget.id)}
+        >
           {label}
         </button>
       ))}
